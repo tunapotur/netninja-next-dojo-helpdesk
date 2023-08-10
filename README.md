950e2a513c7df62130566a6a09d003d3b883f083
Ahmet Tuna POTUR <tpotur@gmail.com>
2023-08-10 19:28:24 +0300

    Next.js 13 Baiscs

--- README.md
+++ README.md
@@ -35,3 +35,12 @@ You can check out [the Next.js GitHub repository](https://github.com/vercel/next
 The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
 
 Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+
+
+## Ders Notları
+npm install json-server -g  // json-server kurulumu
+json-server --watch _data/db.json --port 4000 // json-server çalıştırma
+http://localhost:4000/tickets
+
+// imitate delay
+await new Promise((resolve) => setTimeout(resolve, 3000));
---
+++ app/tickets/TicketList.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import React from "react";
+
+async function getTickets() {
+  const res = await fetch("http://localhost:4000/tickets", {
+    next: {
+      // aysı sayfa içinde 30 saniyede bir çekilecek veriyi güncelleyecek
+      // 30 saniye içinde veri değişse bile cache'den eski veri gelecek
+      // revalidate: 30,
+      revalidate: 0,
+    },
+  });
+
+  return res.json();
+}
+
+export default async function TicketList() {
+  const tickets = await getTickets();
+
+  return (
+    <>
+      {tickets.map((ticket) => (
+        <div key={ticket.id} className="card my-5">
+          <Link href={`/tickets/${ticket.id}`}>
+            <h3>{ticket.title}</h3>
+            <p>{ticket.body.slice(0, 200)}...</p>
+            <div className={`pill ${ticket.priority}`}>
+              {ticket.priority} priority
+            </div>
+          </Link>
+        </div>
+      ))}
+      {tickets.length === 0 && (
+        <p className="text-center">There are no open tickets, yay!</p>
+      )}
+    </>
+  );
+}
